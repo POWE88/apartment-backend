163bacf171e3139bea3ff4c722d32054481b5198
Evan Powell <powellevan88@gmail.com>
2018-11-27 16:04:29 -0800

    basic buildout complete. All working.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+apartment1 = [
+  {street: '7th and J',
+    city: "San Diego",
+    zipcode: "92101",
+    state: "CA",
+    country:"USA",
+    managername: "Doug",
+    managerphone: "619-999-9991",
+    managerhours: "0900-1700"
+
+  }]
+
+apartment1.each do |attributes|
+  Apartment.create(attributes)
+end
+
+#street, city, zipcode, state, country, managername, managerphone, managerhours
